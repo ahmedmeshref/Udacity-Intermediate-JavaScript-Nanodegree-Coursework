846a4c8c9e74be8e216138313cbf83722b7d78c5
ahmedmeshref <a.meshref@alustudent.com>
2021-05-25 19:28:31 +0200

    Solve OOP challenge

---
+++ OOP/classes.js
@@ -0,0 +1,22 @@
+function Animal (name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+// Extend Animal prototype 
+Animal.prototype.walk = function () {
+    return `${this.name} is walking.`;
+}
+
+Animal.prototype.getAge = function () {
+    return `${this.name} is ${this.age} years old.`;
+}
+
+// Create a new instance 
+const biso = new Animal('biso', 1);
+
+// Use biso's methods and properties
+console.log(biso.__proto__ == Animal.prototype);
+console.log(biso.age);
+console.log(biso.getAge());
+console.log(biso.walk());
