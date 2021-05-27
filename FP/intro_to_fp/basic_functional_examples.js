e37c97c6499be632c689e674196ed3ecaf824102
ahmedmeshref <a.meshref@alustudent.com>
2021-05-27 05:20:35 +0200

    Solve basic FP exercise

--- FP/intro_to_fp/basic_functional_examples.js
+++ FP/intro_to_fp/basic_functional_examples.js
@@ -5,8 +5,34 @@
 // Hint - this will help with time of day: new Date().getHours()
 // -----------------------------------------------------------------
 
+/** 
+ * Get the current hour.  
+ */
+function getHour() {
+    let timeNow = new Date().getHours();
+    return timeNow;
+}
 
+/**
+ * Parse the current time and return "good afternoon" is its afternoon and "good morning" otherwise.  
+ */
+function determineGreeting() {
+    let time = getHour();
+    if (time >= 12 && time <= 3) {
+        return "good afternoon";
+    } else {
+        return "good morning";
+    }
+}
 
+/** 
+ * Greet the user with a giving message
+ */
+function greeting(greetMsg) {
+    return greetMsg;
+}
+
+console.log(greeting(determineGreeting()))
 
 // -----------------------------------------------------------------
 // Exercise 2
@@ -15,4 +41,12 @@
 //       zero.
 // -----------------------------------------------------------------
 
+function countDown(number) {
+    let numsArr = [];
+    for (let i = number; i >= 0; i--) {
+        numsArr.push(i);
+    }
+    return numsArr;
+}
 
+console.log(countDown(10))
