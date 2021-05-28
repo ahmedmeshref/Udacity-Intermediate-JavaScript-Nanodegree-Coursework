e6bfcd913c0470c2cd806e5d6cd533c1d1b5ac9b
ahmedmeshref <a.meshref@alustudent.com>
2021-05-28 05:34:58 +0200

    Solve 3 challenges with arr.map

--- FP/functional_js_syntax/array_map.js
+++ FP/functional_js_syntax/array_map.js
@@ -2,22 +2,42 @@
 const start = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // your code
+function reverseArray(arr) {
+    let reversed = arr.map((val, ind) => {
+
+    })
+}
 
 // expected output: Array [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 
 // ----------------------------------------------------------
 // 2. Write a map function to print the Job: Name:
-const shipMates = [["Mal", "Captain"], ["Wash", "Pilot"], ["Zoey", "1st Mate"], ["Jayne", "Public Relations"]]
 
 // your code
+function jobName(shipMates) {
+    let jobNameArr = shipMates.map(val => `${val[1]}: ${val[0]}`);
+    return jobNameArr;
+}
+
+const shipMates = [["Mal", "Captain"], ["Wash", "Pilot"], ["Zoey", "1st Mate"], ["Jayne", "Public Relations"]]
+console.log(jobName(shipMates));
 
 // expected output: Array ["Captain: Mal", etc...]
 
 // ----------------------------------------------------------
-// 3. Write a map function that prints the name: even|odd
-const awayTeam = ["Picard", "Riker", "Troy", "Data"]
+// 3. Write a map function that prints the name length: even|odd
 
-// your code
+function oddOrEvenCalc(val) {
+    return val % 2 == 0 ? 'even' : 'odd';
+}
+
+function oddOrEven(strs) {
+    let strsArr = strs.map(str => `${str}: ${oddOrEvenCalc(str.length)}`);
+    return strsArr;
+}
+
+const awayTeam = ["Picard", "Riker", "Troy", "Data"]
+console.log(oddOrEven(awayTeam));
 
 // expected output: Array: ["Picard: even", "Riker: odd", etc...]
 
@@ -26,8 +46,11 @@ const awayTeam = ["Picard", "Riker", "Troy", "Data"]
 
 const sci_fi_shows = ['Manedlorian', 'Enterprise', 'Firefly', 'Battlestar Galactica']
 
-// your code
+function eleInd(arr) {
+    return arr.map((val, ind) => [val , ind]);
+}
 
+console.log(eleInd(sci_fi_shows));
 // expected output: Array [['Manedlorian', 0], ['Enterprise', 1], ['Firefly', 2], ['Battlestar Galactica', 3]]
 
 // ----------------------------------------------------------
