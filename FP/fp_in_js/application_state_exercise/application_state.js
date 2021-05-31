0c2542e838e925df90e42f007cdae4434585b04c
ahmedmeshref <a.meshref@alustudent.com>
2021-05-31 05:30:25 +0200

    Update the state of an application using FP

--- FP/fp_in_js/application_state_exercise/application_state.js
+++ FP/fp_in_js/application_state_exercise/application_state.js
@@ -4,7 +4,8 @@ let store = {
     user: {
         first_name: 'John',
         last_name: 'Doe'
-    }
+    },
+
 }
 
 const render = (root, state) => {
@@ -22,13 +23,23 @@ const App = (state) => {
    `
 }
 
-window.addEventListener('load', () => {
-    render(root, store)
-})
 
 // But what if state changes at some point in our app?
 // Using the object methods we have learned so far in this course, 
 // create a function called updateStore that takes in the old state, the new state, and updates the old state with any new information
 // This won't be a pure function, instead of a return, call the render method again
 
-// Your Code
+const updateStore = (oldState, newState) => {
+    Object.assign(oldState, newState);
+    render(root, newState);
+}
+
+window.addEventListener('load', () => {
+    render(root, store);
+    updateStore(store, {
+        user: {
+            first_name: 'Ahmed',
+            last_name: 'Meshref'
+        }
+    });
+})
