b679c85951cc9e1e8ed6b80113fc41221f2f6a66
ahmedmeshref <a.meshref@alustudent.com>
2021-05-31 16:58:29 +0200

    Use immutable for state management

--- FP/further_with_funcational_js/implement_immutablejs.js
+++ FP/further_with_funcational_js/implement_immutablejs.js
@@ -1,15 +1,16 @@
 // implement_immutablejs.js
+const Immutable = require('immutable');
 
 // Lets think about how we could refactor our application state flow to use Immutable. 
 // The original code is provided below, how might you re-write both the store object and updateStore function with Immutable?
 // Remember if you need help, the docs should be the first place you look.
 
-let store = {
-    user: {
+let store = Immutable.Map({
+    user: Immutable.Map({
         first_name: 'John',
         last_name: 'Doe'
-    }
-}
+    }),
+})
 
 const updateStore = (store, newState) => {
     store = Object.assign(store, newState)
