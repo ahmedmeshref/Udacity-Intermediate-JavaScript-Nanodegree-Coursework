3a3457cf3d0d7c1f11d8cca69f2051095fac641b
ahmedmeshref <a.meshref@alustudent.com>
2021-06-06 17:32:37 +0200

    Fix error in promise chain - challenge 2

--- AP/promises/PromiseChallenge2.js
+++ AP/promises/PromiseChallenge2.js
@@ -3,30 +3,31 @@
 // This promise chain catch clause always runs. Figure out why and get the console.log at the last then clause to run.
 const setCategory = (category, id) => {
     // imagine we are assiging a category to an item...
-    console.log("Category assigned!")
+    console.log("Category assigned!");
 }
 const data = {
     id: "KDF8D903N",
     intVal: 855,
     message: "This is a message",
     sourceId: "NotNull"
-}
+};
 
 new Promise((resolve, reject) => {
-    resolve(data)
+    resolve(data);
 })
     .then(data => {
-        if (data.soucreId && data.soucreId !== null) {
+        if (data.sourceId) {
             return data;
         }
         // when the if statement returns something, there is no need for an else statement
         throw new Error('No source was defined');
     })
     .then(data => {
-        const { intVal } = data
-        if(intVal > 0 && intVal !== null) {
-            const category = data.intVal.toString().split()[0];
-            setCategory(category, data.id)
+        const {intVal, id, message} = data;
+        if (intVal > 0 && intVal !== null) {
+            const category = intVal.toString().split()[0];
+            setCategory(category, id);
+            return { id, message };
         } else {
             throw new Error('No integer value was provided');
         }
