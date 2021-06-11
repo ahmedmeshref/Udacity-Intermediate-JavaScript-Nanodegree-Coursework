9d19003f84d7070112277956b88376162cdf2e2f
ahmedmeshref <a.meshref@alustudent.com>
2021-06-12 00:42:52 +0200

    Add first class function exercises

---
+++ OOP/1_functions_at_runtime/firstClass_functions.js
@@ -0,0 +1,34 @@
+// Functions are first class in JS which means they could be:
+// 1- Passed to other functions (callbacks). 
+
+function getPositiveValues(arr, callback) {
+    arr.forEach(val => {
+        console.log(callback(val));
+    })
+}
+
+function isPositive(val) {
+    return val > 0 ? val : false;
+}
+
+getPositiveValues([1, 2, -7, 0, 5], isPositive);
+
+// 2- Returned from a function (powerful for closures)
+function counter() {
+    let count = 0;
+    return function incrementCounter() {
+        return count++;
+    }
+}
+
+let myCounter = counter();
+console.log(`Val of my counter: ${myCounter()}`); // 0
+console.log(`Val of my counter: ${myCounter()}`); // 1
+
+
+// 3- Saved in variables 
+let sum = (x, y) => {
+    return x + y;
+}
+
+console.log(`2 + 3 = ${sum(2, 3)}`)
